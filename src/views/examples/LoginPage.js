cbebe39ecae6f076d492ec2b4c495d4f1d867ecb
root <root@HAMIDOU>
2024-05-11 15:18:36 +0100

    first commit

---
+++ src/views/examples/LoginPage.js
@@ -0,0 +1,240 @@
+import React from "react";
+import classnames from "classnames";
+import axios from "axios";
+
+import { useNavigate } from "react-router-dom";
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardImg,
+  CardTitle,
+  Label,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import Footer from "components/Footer/Footer.js";
+
+export default function LoginPage() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [squares1to6, setSquares1to6] = React.useState("");
+  const [squares7and8, setSquares7and8] = React.useState("");
+  const [emailFocus, setEmailFocus] = React.useState(false);
+  const [passwordFocus, setPasswordFocus] = React.useState(false);
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    const userData = {
+      email,
+      password,
+    };
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/auth/login",
+        userData
+      );
+
+      // Assuming response.data contains user and token properties
+      const { user, token } = response.data;
+
+      console.log("Login successful:", user);
+
+      // Store the token in localStorage or cookies for future use
+      // Example using localStorage:
+      localStorage.setItem("token", token);
+
+      // Redirect to the home page after successful login
+      navigate("/");
+    } catch (error) {
+      console.error("Error during login:", error);
+
+      if (error.response) {
+        console.error("Response data:", error.response.data);
+        console.error("Response status:", error.response.status);
+
+        // Add logic to display appropriate error messages to the user
+      } else if (error.request) {
+        console.error("Request error:", error.request);
+      } else {
+        console.error("Error message:", error.message);
+      }
+    }
+  };
+
+  React.useEffect(() => {
+    document.body.classList.toggle("register-page");
+    document.documentElement.addEventListener("mousemove", followCursor);
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      document.body.classList.toggle("register-page");
+      document.documentElement.removeEventListener("mousemove", followCursor);
+    };
+  }, []);
+  const followCursor = (event) => {
+    let posX = event.clientX - window.innerWidth / 2;
+    let posY = event.clientY - window.innerWidth / 6;
+    setSquares1to6(
+      "perspective(500px) rotateY(" +
+        posX * 0.05 +
+        "deg) rotateX(" +
+        posY * -0.05 +
+        "deg)"
+    );
+    setSquares7and8(
+      "perspective(500px) rotateY(" +
+        posX * 0.02 +
+        "deg) rotateX(" +
+        posY * -0.02 +
+        "deg)"
+    );
+  };
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="wrapper">
+        <div className="page-header">
+          <div className="page-header-image" />
+          <div className="content">
+            <Container>
+              <Row>
+                <Col className="offset-lg-0 offset-md-3" lg="5" md="6">
+                  <div
+                    className="square square-7"
+                    id="square7"
+                    style={{ transform: squares7and8 }}
+                  />
+                  <div
+                    className="square square-8"
+                    id="square8"
+                    style={{ transform: squares7and8 }}
+                  />
+                  <Card className="card-register">
+                    <CardHeader>
+                      <CardImg
+                        alt="..."
+                        src={require("assets/img/square-purple-1.png")}
+                      />
+                      <CardTitle tag="h4">Login</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                      <Form className="form">
+                        <InputGroup
+                          className={classnames({
+                            "input-group-focus": emailFocus,
+                          })}
+                        >
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="tim-icons icon-email-85" />
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            placeholder="Email"
+                            type="text"
+                            onChange={(e) => setEmail(e.target.value)}
+                            onFocus={(e) => setEmailFocus(true)}
+                            onBlur={(e) => setEmailFocus(false)}
+                          />
+                        </InputGroup>
+                        <InputGroup
+                          className={classnames({
+                            "input-group-focus": passwordFocus,
+                          })}
+                        >
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="tim-icons icon-lock-circle" />
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            placeholder="Password"
+                            type="password" // Change this line
+                            onChange={(e) => setPassword(e.target.value)}
+                            onFocus={(e) => setPasswordFocus(true)}
+                            onBlur={(e) => setPasswordFocus(false)}
+                          />
+                        </InputGroup>
+
+                        <FormGroup check className="text-left">
+                          <Label check>
+                            <Input type="checkbox" />
+                            <span className="form-check-sign" />I agree to the{" "}
+                            <a
+                              href="#pablo"
+                              onClick={(e) => e.preventDefault()}
+                            >
+                              terms and conditions
+                            </a>
+                            .
+                          </Label>
+                        </FormGroup>
+                      </Form>
+                    </CardBody>
+                    <CardFooter>
+                      <Button
+                        className="btn-round"
+                        color="primary"
+                        size="lg"
+                        onClick={handleLogin}
+                      >
+                        Login
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                </Col>
+              </Row>
+              <div className="register-bg" />
+              <div
+                className="square square-1"
+                id="square1"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-2"
+                id="square2"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-3"
+                id="square3"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-4"
+                id="square4"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-5"
+                id="square5"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-6"
+                id="square6"
+                style={{ transform: squares1to6 }}
+              />
+            </Container>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+}
